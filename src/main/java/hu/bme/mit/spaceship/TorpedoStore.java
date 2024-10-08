430a8a972b501cf4cd0bb0b307e7a5a3eec3157e
akito0120 <akito.ky.120@gmail.com>
2024-10-08 23:20:45 +0200

    Save and reuse random generator in TorpedoStore

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,8 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  private Random generator;
+
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
@@ -26,6 +28,8 @@ public class TorpedoStore {
         FAILURE_RATE = 0.0;
       }
     }
+
+    generator = new Random();
   }
 
   public boolean fire(int numberOfTorpedos){
@@ -36,7 +40,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
