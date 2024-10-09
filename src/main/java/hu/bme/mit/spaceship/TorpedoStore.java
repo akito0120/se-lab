f2a0e3f2e7b9fb796766eb9f36fed2ddccfebe4d
akito0120 <akito.ky.120@gmail.com>
2024-10-09 13:22:17 +0200

    Initialize generator inline

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator;
+  private Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
@@ -29,7 +29,6 @@ public class TorpedoStore {
       }
     }
 
-    generator = new Random();
   }
 
   public boolean fire(int numberOfTorpedos){
